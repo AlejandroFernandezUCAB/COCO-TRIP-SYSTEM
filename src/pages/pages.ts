cf12e36e0d200d8db5cf28a9dd233a9410767021
marguelles <marguelles@transamovil.com>
2017-10-15 13:58:26 -0400

    cocotrip-ionic-app

--- src/pages/pages.ts
+++
@@ -1,6 +0,0 @@
-export * from './login/login';
-export * from './perfil/perfil';
-export * from './amistades-grupos/amistades-grupos';
-export * from './eventos-actividades/eventos-actividades';
-export * from './itinerario/itinerario';
-export * from './chat/chat';
